3ec261022301250fb98cfcc16e1577a7d5ad8f2c
Tommy Long <pete_201@hotmail.com>
2017-03-10 06:55:24 +0000

    added db initialization todo for discussion with @herecydev

--- test/IdentityServer4.MongoDB.IntegrationTests/DatabaseProviderFixture.cs
+++ test/IdentityServer4.MongoDB.IntegrationTests/DatabaseProviderFixture.cs
@@ -7,6 +7,9 @@ namespace IdentityServer4.MongoDB.IntegrationTests
 {
 	public class DatabaseProviderFixture : IDisposable
 	{
+		// TODO: Find a way to intialize MongoDB
+		// Given we don't have the luxury of EF migrations, initialization, nor seeds, 
+		// contemplating using some docker integration, such as https://github.com/mariotoffia/FluentDocker
 		public void Dispose()
 		{
 		}
