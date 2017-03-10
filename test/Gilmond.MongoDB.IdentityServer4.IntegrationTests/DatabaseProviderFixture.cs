c282fecbb7f379ed9e4ee0e9701cf4301177e071
Tommy Long <pete_201@hotmail.com>
2017-03-10 08:20:50 +0000

    corrected naming as per is4 contribution guidelines

--- test/Gilmond.MongoDB.IdentityServer4.IntegrationTests/DatabaseProviderFixture.cs
+++ test/Gilmond.MongoDB.IdentityServer4.IntegrationTests/DatabaseProviderFixture.cs
@@ -1,9 +1,6 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+﻿using System;
 
-using System;
-
-namespace IdentityServer4.MongoDB.IntegrationTests
+namespace Gilmond.MongoDB.IdentityServer4.IntegrationTests
 {
 	public class DatabaseProviderFixture : IDisposable
 	{
---
+++ test/Gilmond.MongoDB.IdentityServer4.IntegrationTests/Stores/ClientStoreTests.cs
@@ -0,0 +1,8 @@
+﻿using Xunit;
+
+namespace Gilmond.MongoDB.IdentityServer4.IntegrationTests
+{
+    public class ClientStoreTests : IClassFixture<DatabaseProviderFixture>
+    {
+    }
+}
